a84d33b260f3b86bffbfd163751bafd6ecef3804
Orlando <teodoront@gmail.com>
2024-09-02 21:48:29 -0300

    response e model movies

---
+++ app/src/main/java/br/com/movieapp/core/data/remote/model/SpokenLanguage.kt
@@ -0,0 +1,14 @@
+package br.com.movieapp.core.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SpokenLanguage(
+    @SerializedName("english_name")
+    val english_name: String,
+
+    @SerializedName("iso_639_1")
+    val iso_639_1: String,
+
+    @SerializedName("name")
+    val name: String
+)
