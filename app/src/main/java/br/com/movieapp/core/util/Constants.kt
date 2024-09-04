639b1854d0b351e6b92daef291ca13209bfdd4eb
Orlando <teodoront@gmail.com>
2024-09-03 21:45:36 -0300

    Inerface, interceptor,  Hilt e Timber

---
+++ app/src/main/java/br/com/movieapp/core/util/Constants.kt
@@ -0,0 +1,7 @@
+package br.com.movieapp.core.util
+
+object Constants {
+    const val API_KEY_PARAM = "api_key"
+    const val LANGUAGE_PARAM = "languange"
+    const val LANGUAGE_VALUE = "pt-BR"
+}
