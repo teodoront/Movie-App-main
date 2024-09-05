dfae763849767086611ff1dd753ac41395220c17
Orlando <teodoront@gmail.com>
2024-09-04 21:22:53 -0300

    Utils

---
+++ app/src/main/java/br/com/movieapp/core/util/Extensions.kt
@@ -0,0 +1,6 @@
+package br.com.movieapp.core.util
+
+import br.com.movieapp.BuildConfig
+
+fun String?.toPostUrl() = "${BuildConfig.BASE_URL}$this"
+fun String?.toBackdropUrl() = "${BuildConfig.BASE_URL_IMAGE}$this"
